48ee4b05cef9b905d085a94de1babd860927bee8
Justin Vermillion <vermillion@lanl.gov>
2019-04-18 11:22:26 -0600

    mod to genkey() function

--- keyserver.py
+++ keyserver.py
@@ -1,10 +1,13 @@
-from os import chmod
+from os import path, chmod
 from Crypto.PublicKey import RSA
 
-key = RSA.generate(2048)
-with open("/tmp/private.key", 'wb') as content_file:
-    chmod("/tmp/private.key", 0o600)
-    content_file.write(key.exportKey('PEM'))
-pubkey = key.publickey()
-with open("/tmp/public.key", 'wb') as content_file:
-    content_file.write(pubkey.exportKey('PEM'))
+keydir = './keys/'
+
+def genkey():
+    key = RSA.generate(2048)
+    with open(path.join(keydir, "private.key"), 'wb') as content_file:
+        chmod("/tmp/private.key", 0o600)
+        content_file.write(key.exportKey('PEM'))
+    pubkey = key.publickey()
+    with open(path.join(keydir, "public.key"), 'wb') as content_file:
+        content_file.write(pubkey.exportKey('PEM'))
