15e7ddeeff2620a55fb1285278ff170b0f772dc4
Justin Vermillion <justin@lockboxsecurity.net>
2019-04-17 18:47:31 -0600

    fixed an issue with octal literal

--- keyserver.py
+++ keyserver.py
@@ -3,8 +3,8 @@ from Crypto.PublicKey import RSA
 
 key = RSA.generate(2048)
 with open("/tmp/private.key", 'wb') as content_file:
-    chmod("/tmp/private.key", 0600)
+    chmod("/tmp/private.key", 0o600)
     content_file.write(key.exportKey('PEM'))
 pubkey = key.publickey()
 with open("/tmp/public.key", 'wb') as content_file:
-    content_file.write(pubkey.exportKey('OpenSSH'))
+    content_file.write(pubkey.exportKey('PEM'))
